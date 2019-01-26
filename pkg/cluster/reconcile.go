295a474ed9b88c9c1eb5241b8873e6978d54aecc
Alan Mock <alan.mock@objectrocket.com>
2019-01-26 11:23:33 -0800

    Update reconcile

--- pkg/cluster/reconcile.go
+++ pkg/cluster/reconcile.go
@@ -16,9 +16,11 @@ package cluster
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/objectrocket/sensu-operator/pkg/util/k8sutil"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // ErrLostQuorum indicates that the etcd cluster lost its quorum.
@@ -28,6 +30,26 @@ var ErrLostQuorum = errors.New("lost quorum")
 // - it tries to reconcile the cluster to desired size.
 // - if the cluster needs for upgrade, it tries to upgrade old member one by one.
 func (c *Cluster) reconcile(pods []*v1.Pod) error {
+	if c.cluster.Spec.Size != c.statefulSet.Size() {
+		set, err := c.config.KubeCli.AppsV1beta1().StatefulSets(c.statefulSet.GetNamespace()).Get(c.statefulSet.GetName(), metav1.GetOptions{})
+		if err != nil {
+			return fmt.Errorf("Error getting StatefulSet %s for size update: %v", c.statefulSet.GetName(), err)
+		}
+		set, err = c.config.KubeCli.AppsV1beta1().StatefulSets(c.statefulSet.GetNamespace()).Update(set)
+		if err != nil {
+			return fmt.Errorf("Error updating StatefulSet %s size: %v", c.statefulSet.GetName(), err)
+		}
+		c.statefulSet = set
+		c.logger.Infof("Update StatefulSet %s size to %d", c.statefulSet.GetName(), c.statefulSet.Size())
+		return nil
+	}
+	var oldPod *v1.Pod
+	oldPod = pickOneOldMember(pods, c.cluster.Spec.Version)
+	if oldPod != nil {
+		// This needs to be handled once the etcd cluster is either external or has multiple nodes
+		c.logger.Warnf("Pod %s needs upgraded from version %s to %s", oldPod.GetName(), k8sutil.GetSensuVersion(oldPod), c.cluster.Spec.Version)
+		return nil
+	}
 	return nil
 }
 
