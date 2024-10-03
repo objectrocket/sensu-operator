b4e5b9a75b1d26c5dbcdc0f4b9a678510bbf65c6
Biswajit Mohanty <bisw8246@Biswajits-MacBook-Pro.local>
2024-10-04 00:12:43 +0530

    spec

--- pkg/util/k8sutil/conversionutil/crdvalidation.go
+++ pkg/util/k8sutil/conversionutil/crdvalidation.go
@@ -3,10 +3,10 @@
 package crdvalidation
 
 import (
-	spec "github.com/go-openapi/spec"
 	extensionsobj "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	common "k8s.io/kube-openapi/pkg/common"
+	spec "k8s.io/kube-openapi/pkg/validation/spec"
 )
 
 // CustomResourceDefinitionTypeMeta set the default kind/apiversion of CRD
